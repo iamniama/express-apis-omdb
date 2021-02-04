d4432db148976904677598020a58be00ce5be68d
iamniama <plectrumonthespectrum@gmail.com>
2021-02-04 16:12:27 -0700

    data models for faves

---
+++ models/favorite.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class favorite extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  favorite.init({
+    title: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    image: DataTypes.TEXT,
+    plot: DataTypes.TEXT,
+    IMDB: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'favorite',
+  });
+  return favorite;
+};
